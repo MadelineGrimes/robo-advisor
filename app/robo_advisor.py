b3498f6ee8f9ba4f6d0d869ff4ecca1b29c82a8f
Madeline Grimes <mkg420@stern.nyu.edu>
2021-06-20 11:16:28 -0400

    Setup the repo

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -4,12 +4,12 @@ import requests
 
 load_dotenv()
 
-ALPHA_VANTAGE_API_KEY = os.getenv("ALPHA_VANTAGE_API_KEY")
+ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
 
 
 
 symbol = "MSFT"
-request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={ALPHA_VANTAGE_API_KEY}"
+request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}"
 
 response = requests.get(request_url)
 
